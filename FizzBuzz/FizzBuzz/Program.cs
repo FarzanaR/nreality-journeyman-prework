7b35dee616c291d8c53fbae50099e3986021ab9e
frassul <frassul>
2014-02-06 16:18:04 +0200

    Initial commit

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 101; i++)
+            {
+                if (i.IsFizz() && i.IsBuzz())
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i.IsFizz())
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i.IsBuzz())
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        public static bool IsFizz(this int number)
+        {
+            return number % 3 == 0 ? true : false;
+        }
+
+        public static bool IsBuzz(this int number)
+        {
+            return number % 5 == 0 ? true : false;
+        }
+
+    }
+}
---
+++ FizzBuzz/FizzBuzzTests/FizzBuzzTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FizzBuzz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void TestFizz()
+        {
+            int number = 3;
+            bool result = number.IsFizz();
+            Assert.IsTrue(result);
+
+            number = 5;
+            result = number.IsFizz();
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestBuzz()
+        {
+            int number = 5;
+            bool result = number.IsBuzz();
+            Assert.IsTrue(result);
+
+            number = 3;
+            result = number.IsBuzz();
+            Assert.IsFalse(result);
+        }
+
+    }
+}
