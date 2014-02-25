d49c139ff1e92b42c6ca9e58806b7f7a1064463b
frassul <farzana.rassul@gmail.com>
2014-02-25 20:13:12 +0200

    Data structures initial commit

---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class CustomQueue
+    {
+        private List<object> _queue;
+
+        public List<object> queue
+        {
+            get { return this._queue; }
+ 
+            set { this._queue = value; }
+        }
+ 
+        public void PUSH(object newObject)
+        {
+            List<object> temp = queue;
+            temp.Add(newObject);
+            queue = temp;
+        }
+
+        public void PULL()
+        {
+            int count = queue.Count;
+            var temp = queue;
+            temp.RemoveAt(count - 1);
+            queue = temp;
+        }
+
+        public void PULL(int index)
+        {
+            List<object> tQueue = queue;
+            tQueue.RemoveAt(index);
+            queue = tQueue;
+        }
+
+        public int SEARCH(object item)
+        {
+            return queue.IndexOf(item);
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DataStructures/DataStructuresTest/QueueTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructures;
+using System.Collections.Generic;
+
+namespace DataStructuresTest
+{
+    [TestClass]
+    public class QueueTests
+    {
+        CustomQueue _customQueue;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _customQueue = new CustomQueue();
+        }
+
+        [TestMethod]
+        public void TestPush()
+        {
+            var queue = _customQueue.queue = new List<object>{1, 2};
+
+            Assert.AreEqual(queue.Count, 2);
+
+            _customQueue.PUSH(3);
+
+            Assert.AreEqual(queue.Count, 3);
+       
+        }
+
+        [TestMethod]
+        public void TestPull()
+        {
+            var queue = _customQueue.queue = new List<object> { 1, 2, 3, 4, 5, 6 };
+
+            Assert.AreEqual(queue.Count, 6);
+
+            _customQueue.PULL();
+
+            Assert.AreEqual(queue.Count, 5);
+
+            Assert.IsFalse(queue.Contains(6));
+        }
+
+        [TestMethod]
+        public void TestPullFromIndex()
+        {
+            var queue = _customQueue.queue = new List<object> { 1, 2, 3, 4, 5, 6 };
+
+            Assert.AreEqual(queue.Count, 6);
+
+            _customQueue.PULL(3);
+
+            Assert.AreEqual(queue.Count, 5);
+
+            Assert.IsFalse(queue.Contains(4));
+        }
+
+        [TestMethod]
+        public void TestSearch()
+        {
+            var queue = _customQueue.queue = new List<object> { "John", "Jane", "Mary", "Joe"};
+
+            int index = _customQueue.SEARCH("Mary");
+
+            Assert.AreEqual(index, 2);
+        }
+    }
+}
